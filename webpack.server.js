d075237a5845c0712b578e56b053eb62be102d0e
Farid Ansari <farid12ansari7@gmail.com>
2017-12-31 17:58:17 +0530

    export by merging base webpack config with server or client side webpack config

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
+const config = {
   //Inform webpack that we are building bundle
   //for nodeJS, rather than for a browser
   target: 'node',
@@ -12,23 +14,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  },
-
-  //Tell babel to run on every file it run through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(baseConfig, config);
