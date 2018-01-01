f8657b4efecbbbae250a96a51ae12ddbb93807e4
Farid Ansari <farid12ansari7@gmail.com>
2018-01-01 06:01:16 +0530

    added redux store to server side

--- src/client/client.js
+++ src/client/client.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './components/Routes';
 import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 //can use import as webpack is run on this file and babel will convert it to it's default form during compliation
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+  //some logic to initialize the store
+  //and load some data to the store
+  res.send(renderer(req, store));
 });
 app.listen(3000, () => {
   console.log('Port 3000');
